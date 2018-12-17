c9e620bac6c9d861e2561552e7e63e9c1bcd6b30
kimia-r <45943277+kimia-r@users.noreply.github.com>
2018-12-17 19:03:10 +0330

    Create check_2.ino

---
+++ check_2.ino
@@ -0,0 +1,14 @@
+ void check_2(String num){
+      
+      if(num.equals(one_string)){
+      Serial.println("Number one is on.");
+      }
+      
+      else if(num.equals("2")){
+      Serial.println("Number two is on.");
+      }
+      
+      else if(num.equals("3")){
+      Serial.println("Number three is on.");
+      }
+ }
