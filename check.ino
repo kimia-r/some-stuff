901a6c0fa9945bbf323405c7df18a519a8ee6400
kimia-r <45943277+kimia-r@users.noreply.github.com>
2018-12-17 19:03:05 +0330

    Create check.ino

---
+++ check.ino
@@ -0,0 +1,18 @@
+ void check(int num){
+  
+ 
+ switch (num){
+      
+      case 1:
+      Serial.println("Number one is on.");
+      break;
+      
+      case 2:
+      Serial.println("Number two is on.");
+      break;
+      
+      case 3:
+      Serial.println("Number three is on.");
+      break;
+      }
+ }
