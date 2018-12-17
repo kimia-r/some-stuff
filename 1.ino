45cfefd0323ee240be1d0176c07b0a6b8585c192
kimia-r <45943277+kimia-r@users.noreply.github.com>
2018-12-17 19:02:56 +0330

    Create 1.ino

---
+++ 1.ino
@@ -0,0 +1,15 @@
+String one_string = "1";
+
+void setup() {
+  
+  Serial.begin(9600);
+  Serial.println("Please enter a number from 1 to 3");
+  
+}
+
+void loop() {
+Serial.flush();
+  
+Get_data_2();
+
+}
